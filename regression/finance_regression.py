cf72634493b265a28821cfe289f7efcdf6f7450b
skl92 <stanleykliu92@gmail.com>
2016-04-22 15:27:03 -0700

    complete regression project lesson 6

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -19,8 +19,9 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
-### list the features you want to look at--first item in the 
+### list the features you want to look at--first item in the
 ### list will be the "target" feature
+
 features_list = ["bonus", "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
@@ -29,28 +30,33 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
-### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
-### plots it correctly. Don't forget to change the test_color above from "b" to
-### "r" to differentiate training points from test points.
+### your regression goes here!
+### please name it reg, so that the plotting code below picks it up and
+### plots it correctly
+
+from sklearn.linear_model import LinearRegression
 
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print "Coeff: ", reg.coef_
+print "Intercept: ", reg.intercept_
+print "Score: ", reg.score(feature_train, target_train)
+print "Score: ", reg.score(feature_test, target_test)
 
-
-
-
+pred = reg.predict(feature_test)
 
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
+    plt.scatter( feature, target, color=test_color )
 for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
+    plt.scatter( feature, target, color=train_color )
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
@@ -64,6 +70,16 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+# added
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="r")
+print "*********** Switch **********"
+print "Coeff: ", reg.coef_
+print "Intercept: ", reg.intercept_
+print "Score: ", reg.score(feature_train, target_train)
+print "Score: ", reg.score(feature_test, target_test)
+#
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
