34693252cc48f828d0dcb71e5e866d1cc0601854
skl92 <stanleykliu92@gmail.com>
2016-05-15 23:11:34 -0700

    complete text learning project

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -9,17 +9,13 @@ sys.path.append( "../tools/" )
 from parse_out_email_text import parseOutText
 
 """
-    Starter code to process the emails from Sara and Chris to extract
-    the features and get the documents ready for classification.
-
-    The list of all the emails from Sara are in the from_sara list
+    starter code to process the emails from Sara and Chris to extract
+    the features and get the documents ready for classification
+    the list of all the emails from Sara are in the from_sara list
     likewise for emails from Chris (from_chris)
-
-    The actual documents are in the Enron email dataset, which
-    you downloaded/unpacked in Part 0 of the first mini-project. If you have
-    not obtained the Enron email corpus, run startup.py in the tools folder.
-
-    The data is stored in lists and packed away in pickle files at the end.
+    the actual documents are in the Enron email dataset, which
+    you downloaded/unpacked in Part 0 of the first mini-project
+    the data is stored in lists and packed away in pickle files at the end
 """
 
 
@@ -32,16 +28,14 @@ word_data = []
 ### temp_counter is a way to speed up the development--there are
 ### thousands of emails from Sara and Chris, so running over all of them
 ### can take a long time
-### temp_counter helps you only look at the first 200 emails in the list so you
-### can iterate your modifications quicker
+### temp_counter helps you only look at the first 200 emails in the list
 temp_counter = 0
-
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
@@ -65,9 +59,9 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             else:
                 from_data.append(1)
 
+            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
             email.close()
 
-print word_data[152], "word data"
 
 print "emails processed"
 from_sara.close()
@@ -75,10 +69,6 @@ from_chris.close()
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
-
-
-
-
 
 ### in Part 4, do TfIdf vectorization here
 
